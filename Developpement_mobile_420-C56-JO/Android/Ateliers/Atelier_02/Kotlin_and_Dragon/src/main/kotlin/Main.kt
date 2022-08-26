fea2c98fc97e9928731f5a09c29d215d3dcf0d8f
JGaouette <jeremygaouette@gmail.com>
2022-08-26 17:56:44 -0400

    Adding combat system in Kotlin and Dragon

--- Developpement_mobile_420-C56-JO/Android/Ateliers/Atelier_02/Kotlin_and_Dragon/src/main/kotlin/Main.kt
+++ Developpement_mobile_420-C56-JO/Android/Ateliers/Atelier_02/Kotlin_and_Dragon/src/main/kotlin/Main.kt
@@ -1,66 +1,129 @@
 fun main() {
     var character : Character? = null
+    var inFight = true
 
-    var isGood = false
-    while(!isGood){
-        println("Choose your classe \n1: Warrior \n2: Mage \n" +
+    while(character == null){
+        println("Choose your class \n1: Warrior \n2: Mage \n" +
                 "3: Cleric")
 
-        when (readLine()) {
-            "1" -> {
-                character = Warrior()
-                isGood = true
-            }
-            "2" -> {
-                character = Mage()
-                isGood = true
-            }
-            "3" -> {
-                character = Cleric()
-                isGood = true
-            }
-            else -> {
-                println("This class doesn't exist \n")
-            }
+        character = when (readLine()) {
+            "1" ->  Warrior()
+            "2" -> Mage()
+            "3" ->  Cleric()
+            else -> null
         }
     }
 
+    while(inFight){
+        val monster = Monster()
 
-    println("Class: " + character!!.javaClass.name + "   Health: " + character.health + "/" + character.maxHealth)
+        while (character.health > 0 && monster.health > 0){
+            println("Class: " + character.javaClass.name + "             Class: " + monster.javaClass.name)
+            println("Health: " + character.health + "/" + character.maxHealth + "      VS      Health: " + monster.health + "/" + monster.maxHealth)
+            println("Attack: " + character.attack + "                 Attack: " + monster.attack)
+            println("Defense: " + character.defense + "                Defense: " + monster.defense)
+            println("")
+
+            var action : String? = null
+            while (action == null){
+                if(character.cooldown > 0)
+                    println("1: Attack  |  2: Special Attack (In " + character.cooldown + " turns )  |  3: Flee like a fool")
+                else
+                    println("1: Attack  |  2: Special Attack (Ready)  |  3: Flee like a fool")
+
+                action = readLine()
+
+                when (action) {
+                    "1" -> character.attack(monster)
+                    "2" -> character.special(monster)
+                    "3" -> character.flee(monster)
+                    else -> action = null
+                }
+                println()
+            }
+
+            if(monster.health > 0) monster.attack(character)
+
+            if(character.health <= 0 || monster.health <= 0){
+                if(character.health <= 0){
+                    println("You are DEAD! Shame on you and your family")
+                }
+                if(monster.health <= 0){
+                    println("Good job, you have slain the enemy!")
+                }
+
+                println("Do you want to make an other fight? \"Yes\" to continue, other stuff to be a coward")
+                when (readLine()) {
+                    "Yes" -> character.levelUp()
+                    else-> inFight = false
+                }
+            }
+        }
+    }
 }
 
 abstract class Entity {
     abstract var attack: Int
     abstract var defense: Int
+    abstract var maxHealth : Int
     abstract var health: Int
+    abstract var minDamage: Int
     abstract var maxDamage: Int
 
     open fun attack(against : Entity) {
-        var difficulty = this.attack - against.defense
-        var touch = (0..20).random()
+        val difficulty = this.attack - against.defense
+        val touch = (0..20).random()
 
         if (touch >= difficulty){
-            against.health -= (1..this.maxDamage).random()
+            against.getHit((minDamage..this.maxDamage).random())
         }
+    }
+
+    open fun getHit(damage : Int) {
+        health -= damage
     }
 }
 
 class Monster : Entity() {
     override var attack = (15..20).random()
     override var defense = (2..8).random()
-    override var health = (3..10).random()
+    override var maxHealth = (3..10).random()
+    override var health = maxHealth
+    override var minDamage = 1
     override var maxDamage = 5
 }
 
 abstract class Character : Entity()  {
-    abstract var maxHealth : Int
+    open var cooldown = 3
 
+    override fun attack(against : Entity) {
+        val difficulty = this.attack - against.defense
+        val touch = (0..20).random()
 
-    open fun special(){}
+        if (touch >= difficulty)
+            against.getHit((minDamage..this.maxDamage).random())
+
+        if(cooldown > 0) cooldown--
+    }
+    open fun special(against : Entity){}
 
     open fun flee(against : Entity) {
         against.attack(this)
-        this.health = this.maxHealth
+        heal(maxHealth)
+    }
+
+    open fun heal(hp : Int){
+        this.health += hp
+        if(this.health > this.maxHealth) this.health = this.maxHealth
+    }
+
+    open fun levelUp(){
+        this.attack += 1
+        this.defense += 1
+        this.maxHealth += 1
+        this.health += 1
+        this.minDamage += 1
+        this.maxDamage += 1
     }
 }
 
@@ -69,9 +132,13 @@ class Warrior : Character() {
     override var defense = 2
     override var maxHealth = 10
     override var health = 10
+    override var minDamage = 1
     override var maxDamage = 8
 
-    override fun special(){}
+    override fun special(against : Entity){
+        against.getHit((minDamage..this.maxDamage).random())
+        cooldown += 2
+    }
 }
 
 class Mage : Character() {
@@ -79,9 +146,13 @@ class Mage : Character() {
     override var defense = 8
     override var maxHealth = 5
     override var health = 5
+    override var minDamage = 1
     override var maxDamage = 4
 
-    override fun special(){}
+    override fun special(against : Entity){
+        against.getHit((4..10).random())
+        cooldown += 2
+    }
 }
 
 class Cleric : Character() {
@@ -89,7 +160,11 @@ class Cleric : Character() {
     override var defense = 4
     override var maxHealth = 8
     override var health = 8
+    override var minDamage = 1
     override var maxDamage = 6
 
-    override fun special(){}
+    override fun special(against : Entity){
+        this.heal((2..6).random())
+        cooldown += 2
+    }
 }
