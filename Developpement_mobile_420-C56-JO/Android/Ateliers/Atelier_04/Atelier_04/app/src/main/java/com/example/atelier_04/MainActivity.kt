1f1131aef6fbaa30f65b6849948505a9c7586e34
JGaouette <jeremygaouette@gmail.com>
2022-09-02 16:05:10 -0400

    Working on Atelier_04

--- Developpement_mobile_420-C56-JO/Android/Ateliers/Atelier_04/Atelier_04/app/src/main/java/com/example/atelier_04/MainActivity.kt
+++ Developpement_mobile_420-C56-JO/Android/Ateliers/Atelier_04/Atelier_04/app/src/main/java/com/example/atelier_04/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.atelier_04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switchTaxes : Button = findViewById(R.id.switchTaxes)
+
     }
+
+
 }
