952ed754a430f6f4be3e8c1c2f2042b9488c695f
JGaouette <jeremygaouette@gmail.com>
2022-09-06 17:16:11 -0400

    Android studio first action with button and more

--- Developpement_mobile_420-C56-JO/Android/Ateliers/Atelier_04/Atelier_04/app/src/main/java/com/example/atelier_04/MainActivity.kt
+++ Developpement_mobile_420-C56-JO/Android/Ateliers/Atelier_04/Atelier_04/app/src/main/java/com/example/atelier_04/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.example.atelier_04
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.atelier_04.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var hasTaxes = false
 
-        val switchTaxes : Button = findViewById(R.id.switchTaxes)
+        binding.tgbTaxes.setOnClickListener{
+            hasTaxes = binding.tgbTaxes.isChecked
+        }
 
+        binding.btnCalculate.setOnClickListener{
+            var cost = 0.00
+            if(binding.ednCost.text.toString() != "")
+                cost = binding.ednCost.text.toString().toDouble()
+
+            var tip = binding.spnTip.selectedItem.toString().toDouble() / 100.0
+            var person = binding.spnPerson.selectedItem.toString().toDouble()
+            var taxes = 0.00
+
+            if(hasTaxes) taxes = cost * 0.14975
+
+            var amount = ((cost + taxes) + ((cost + taxes) * tip)) / person
+            val formatted = String.format("%.2f", amount)
+
+            Toast.makeText(this, "$formatted$",Toast.LENGTH_LONG).show()
+        }
     }
-
-
 }
