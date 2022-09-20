447f3c4aa5622ec72f2cd2bc7dbe60e14761bbca
JGaouette <jeremygaouette@gmail.com>
2022-09-19 23:12:51 -0400

    Create thumbnail with a list of images

--- Reseaux_II_420-M54-JO/Ateliers/Atelier_06/numero_02.go
+++ Reseaux_II_420-M54-JO/Ateliers/Atelier_06/numero_02.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	"fmt"
+	"github.com/nfnt/resize"
+	"image/jpeg"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	files, err := os.ReadDir("./img")
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*
+		for _, f := range files {
+			go toThumbnail(f)
+		}
+	*/
+
+	for _, f := range files {
+		if !f.IsDir() {
+			fmt.Println(f.Name())
+
+			file, err := os.Open("./img/" + f.Name())
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			img, err := jpeg.Decode(file)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			m := resize.Resize(80, 80, img, resize.Lanczos3)
+
+			out, err := os.Create("./img/thumbnail/" + strings.ReplaceAll(f.Name(), ".jpg", "") + "_thumbnail.jpg")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			jpeg.Encode(out, m, nil)
+		}
+	}
+
+}
+
+func toThumbnail(entry os.DirEntry) {
+	file, err := os.Open("./img/" + entry.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := resize.Resize(80, 80, img, resize.Lanczos3)
+
+	out, err := os.Create("./img/thumbnail/" + strings.ReplaceAll(entry.Name(), ".jpg", "") + "_thumbnail.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jpeg.Encode(out, m, nil)
+}
